2d5af0035a85a81478860c96a8b774c6d934fc07
Maneth <maneth.kulatunge@mail.mcgill.ca>
2020-06-10 21:21:21 -0400

    Initial scraper

---
+++ webscraper/scraperIndeed.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import pandas as pd
+from bs4 import BeautifulSoup
+
+driver = webdriver.Chrome("./chromedriver")
+
+dataframe = pd.DataFrame(columns=["Title", "Location", "Company", "Salary", "Sponsored", "Description"])
+
+#Step1: Get the page
+driver.get("https://ca.indeed.com/jobs?q=Hourly+Jobs&l=Canada")
+driver.implicitly_wait
+
+all_jobs = driver.find_elements_by_class_name('result')
+
+for job in all_jobs:
+
+    result_html = job.get_attribute('innerHTML')
+    soup = BeautifulSoup(result_html, 'html.parser')
+
+    try:
+        title = soup.find("a",class_="jobtitle").text.replace('\n','')
+    except:
+        title = 'None'
+    
+    sum_div = job.find_element_by_xpath('./div[3]')
+    try:
+        sum_div.click()
+    except:
+        close_button = driver.find_elements_by_class_name('popover-x-button-close')[0]
+        close_button.click()
+        sum_div.click()
+    job_desc = driver.find_element_by_id('vjs-desc').text
+
+    df = df.append({'Title':title})
+
+    print("Got these many results:",df.shape)
+
+df.to_csv("ai.csv",index=False)
