22174e17e621ab12c9150020fca72ff53c41f933
Maneth <maneth.kulatunge@mail.mcgill.ca>
2020-06-12 18:38:56 -0400

    Added more columns/categories for job scrape

--- webscraper/scraperIndeed.py
+++ webscraper/scraperIndeed.py
@@ -6,35 +6,57 @@ driver = webdriver.Firefox(
     executable_path="/Users/admin/Documents/congrego/webscraper/geckodriver")
 
 df = pd.DataFrame(columns=["Title", "Location",
-                           "Company", "Salary", "Sponsored", "Description"])
+                           "Company", "Salary"])
 
-# Step1: Get the page
-driver.get("https://ca.indeed.com/jobs?q=Hourly+Jobs&l=Canada")
-driver.implicitly_wait
+for i in range(0, 50, 10):
+    driver.get("https://ca.indeed.com/jobs?q=Hourly+Jobs&l=Canada&start="+str(i))
+    driver.implicitly_wait
 
-all_jobs = driver.find_elements_by_class_name('result')
+    all_jobs = driver.find_elements_by_class_name('result')
 
-for job in all_jobs:
+    for job in all_jobs:
 
-    result_html = job.get_attribute('innerHTML')
-    soup = BeautifulSoup(result_html, 'html.parser')
+        result_html = job.get_attribute('innerHTML')
+        soup = BeautifulSoup(result_html, 'html.parser')
 
-    try:
-        title = soup.find("a", class_="jobtitle").text.replace('\n', '')
-    except:
-        title = 'None'
+        try:
+            title = soup.find("a", class_="jobtitle").text.replace('\n', '')
+        except:
+            title = 'None'
 
-    sum_div = job.find_element_by_xpath('./div[3]')
-    try:
-        sum_div.click()
-    except:
-        close_button = driver.find_elements_by_class_name(
-            'popover-x-button-close')[0]
-        close_button.click()
-        sum_div.click()
+        try:
+            location = soup.find(
+                "span", class_="location").text.replace('\n', '')
+            print(location)
+        except:
+            location = 'None'
 
-    df = df.append({'Title': title}, ignore_index=True)
+        try:
+            company = soup.find(
+                "span", class_="company").text.replace('\n', '')
+            print(company)
+        except:
+            company = 'None'
 
-    print("Got these many results:", df.shape)
+        try:
+            salary = soup.find(
+                "span", class_="salaryText").text.replace('\n', '')
+            print(salary)
+        except:
+            salary = 'None'
+
+        sum_div = job.find_element_by_xpath('./div[3]')
+        try:
+            sum_div.click()
+        except:
+            close_button = driver.find_elements_by_class_name(
+                'popover-x-button-close')[0]
+            close_button.click()
+            sum_div.click()
+
+        df = df.append({'Title': title, 'Location': location,
+                        'Company': company, 'Salary': salary}, ignore_index=True)
+
+        print("Got these many results:", df.shape)
 
 df.to_csv("ai.csv", index=False)
