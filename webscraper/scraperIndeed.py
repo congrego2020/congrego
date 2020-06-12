a0cc8ee6355b5ad88e64a821cb175857dd82e295
Maneth <maneth.kulatunge@mail.mcgill.ca>
2020-06-11 20:07:05 -0400

    Variable name changes and inclusion of firefox as a use of driver

--- webscraper/scraperIndeed.py
+++ webscraper/scraperIndeed.py
@@ -2,11 +2,13 @@ from selenium import webdriver
 import pandas as pd
 from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome("./chromedriver")
+driver = webdriver.Firefox(
+    executable_path="/Users/admin/Documents/congrego/webscraper/geckodriver")
 
-dataframe = pd.DataFrame(columns=["Title", "Location", "Company", "Salary", "Sponsored", "Description"])
+df = pd.DataFrame(columns=["Title", "Location",
+                           "Company", "Salary", "Sponsored", "Description"])
 
-#Step1: Get the page
+# Step1: Get the page
 driver.get("https://ca.indeed.com/jobs?q=Hourly+Jobs&l=Canada")
 driver.implicitly_wait
 
@@ -18,21 +20,21 @@ for job in all_jobs:
     soup = BeautifulSoup(result_html, 'html.parser')
 
     try:
-        title = soup.find("a",class_="jobtitle").text.replace('\n','')
+        title = soup.find("a", class_="jobtitle").text.replace('\n', '')
     except:
         title = 'None'
-    
+
     sum_div = job.find_element_by_xpath('./div[3]')
     try:
         sum_div.click()
     except:
-        close_button = driver.find_elements_by_class_name('popover-x-button-close')[0]
+        close_button = driver.find_elements_by_class_name(
+            'popover-x-button-close')[0]
         close_button.click()
         sum_div.click()
-    job_desc = driver.find_element_by_id('vjs-desc').text
 
-    df = df.append({'Title':title})
+    df = df.append({'Title': title}, ignore_index=True)
 
-    print("Got these many results:",df.shape)
+    print("Got these many results:", df.shape)
 
-df.to_csv("ai.csv",index=False)
+df.to_csv("ai.csv", index=False)
